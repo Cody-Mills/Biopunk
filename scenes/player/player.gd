152226470e881b85cb53025bfad97913287fe9e8
Cody Mills <CodyMills11@hotmail.com>
2024-09-29 19:56:59 +0100

    Simple First-person movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,7 +3,21 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.004
+@onready var head: Node3D = %Head
+@onready var camera_3d: Camera3D = %Camera3D
+@onready var player: CharacterBody3D = $"."
 
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera_3d.rotate_x(-event.relative.y * SENSITIVITY)
+		camera_3d.rotation.x = clamp(camera_3d.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,7 +31,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
