b349e86adffd4c7f49765ee407c7df2a016e5201
Cody Mills <CodyMills11@hotmail.com>
2024-09-30 11:40:53 +0100

    Crouching Adjustments

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -6,19 +6,22 @@ const SPRINT_SPEED = WALK_SPEED * 1.4
 var is_sprinting : bool = false
 const WALK_SPEED = 5.0
 const CROUCH_SPEED = WALK_SPEED * 0.6
+var is_crouching : bool = false
+const SLIDE_SPEED = WALK_SPEED * 2
+var is_sliding : bool = false
 
 #Jumping
 const JUMP_VELOCITY = 4.5
-var jump_count = 1
-var max_jumps = 1#scaleable
+var jump_count = max_jumps
+var max_jumps = 1 #scaleable
 var jump_available : bool = true
-#var was_on_floor : bool = true #just walked off
 
 const SENSITIVITY = 0.004
 
 @onready var head: Node3D = %Head
 @onready var camera_3d: Camera3D = %Camera3D
 @onready var player: CharacterBody3D = $"."
+
 
 #Head bobbing via Sinewave
 const BOB_FREQ = 2.0
@@ -34,8 +37,7 @@ var t_bob = 0.0
 #-Sliding
 #-Bunny Hopping
 
-#Coyote Time (DO LATER BORING ATM)
-#@onready var coyote_time: Timer = %CoyoteTime
+
 
 
 func _ready() -> void:
@@ -72,15 +74,24 @@ func _physics_process(delta: float) -> void:
 	else:
 		speed = WALK_SPEED
 		is_sprinting = false
-	
+	#Crouching
 	if Input.is_action_pressed("ui_crouch") && !is_sprinting:
 		$CollisionShape3D.scale = Vector3(1, 0.5, 1)
 		speed = CROUCH_SPEED
-	if Input.is_action_just_released("ui_crouch") && !is_sprinting:
+		is_crouching = true
+	if Input.is_action_just_released("ui_crouch"):
 		$CollisionShape3D.scale = Vector3(1, 1, 1)
 		speed = WALK_SPEED
-	if Input.is_action_pressed("ui_crouch") && is_sprinting:
-		pass #slide
+		is_crouching = false
+	#Sliding
+	if Input.is_action_pressed("ui_crouch") && is_sprinting :
+		$CollisionShape3D.scale = Vector3(1, 0.3, 1)
+		speed = SLIDE_SPEED
+		is_sliding = true
+	if Input.is_action_just_released("ui_crouch") && is_sliding:
+		is_sliding = false
+		speed = WALK_SPEED
+		$CollisionShape3D.scale = Vector3(1, 1, 1)
 	
 	#Movement Inputs
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
