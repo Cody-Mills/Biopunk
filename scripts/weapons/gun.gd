0ca19df5169165f008e8efbf3c454b7ed4523c93
Cody Mills <CodyMills11@hotmail.com>
2024-10-05 15:23:50 +0100

    Working on Weapons framework

---
+++ scripts/weapons/gun.gd
@@ -0,0 +1,19 @@
+class_name Gun
+extends Node
+
+
+
+func _fire() -> void:
+	#Get parent node "fire position" 
+	#fire a bullet
+	#play bullet firing effect
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _reload() -> void:
+	pass
+	#if 'ui_reload' pressed or currentAmmo == 0
+	#play reload animation
+	#replensih bullets
+	
